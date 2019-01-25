fe8792b81a9f25a3d6880cd6847c6c2c960a448f
Louise Francois <louise.ban@gmail.com>
2019-01-25 11:22:08 +0100

    add error msg

--- week2/homework/src/removeItem.js
+++ week2/homework/src/removeItem.js
@@ -6,11 +6,16 @@ const fs = require('fs');
 module.exports = {
     removeItem: function(){
        let index = process.argv[3];
-       let data = fs.readFileSync('todolist.txt', 'utf8');
-       let parse = JSON.parse(data);
-       let removed = parse.splice(1, index)
-       fs.writeFileSync('todolist.txt', JSON.stringify(removed), 'utf8');
-       console.log('item removed from your list');
+       let data = fs.readFile('todolist.txt', 'utf8', function (err, data){
+            if (err){
+            console.log('cannot access to-do list');
+            } else {
+            let parse = JSON.parse(data);
+            let removed = parse.splice(index -1, 1);
+            fs.writeFileSync('todolist.txt', JSON.stringify(removed), 'utf8');
+            console.log('item removed from your list');
+            }
+        })
     }
 }
 
--- week2/homework/src/reset.js
+++ week2/homework/src/reset.js
@@ -5,10 +5,15 @@ const fs = require('fs');
 module.exports = {
     reset: function(){
         let index = process.argv[3];
-        let data = fs.readFileSync('todolist.txt', 'utf8');
-        let parse = JSON.parse(data);
-        let removed = parse.splice(index, 0)
-        fs.writeFileSync('todolist.txt', JSON.stringify(removed), 'utf8');
-        console.log('To do List is now reset');
+        let data = fs.readFile('todolist.txt', 'utf8', function (err, data){
+            if (err){
+            console.log('cannot access to-do list');
+            } else {
+            let parse = JSON.parse(data);
+            let removed = parse.splice(index, 0);
+            fs.writeFileSync('todolist.txt', JSON.stringify(removed), 'utf8');
+            console.log('To do List is now reset');
+            }
+        })
      }
 }
